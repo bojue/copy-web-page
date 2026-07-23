f6c769f8ab0961c8093406aaf7420ed2537d5df1
maliang <maliang@sharetek.com.cn>
2026-07-23 14:44:35 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh" className="h-full antialiased">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+// 过滤本地访问统计
+if (typeof window !== 'undefined') {
+  var hostname = window.location.hostname;
+  var isLocal = hostname === 'localhost' ||
+                hostname === '127.0.0.1' ||
+                hostname.startsWith('192.168.') ||
+                hostname.startsWith('10.') ||
+                hostname.startsWith('172.16.');
+
+  if (!isLocal) {
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?8775e1dd8a18a03c5be475ff840c59ff";
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(hm, s);
+    })();
+  }
+}
+            `,
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
